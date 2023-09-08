f410b50757ba8cf53d42a30a928641b12f25ae0b
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-09-09 02:45:28 +0600

    Update models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,10 +2,18 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
-  validates_presence_of :name
+
+  def total_price
+    total = 0
+    foods.each do |food|
+      total += food.price
+    end
+    total
+  end
+
   def missing_foods
     data = { list: [], count: 0, total_price: 0 }
-    recipe_foods.each do |recipe_food|
+    recipe_foods.includes(:food).each do |recipe_food|
       recipe_food_quantity = recipe_food.quantity
       food_inv_quantity = recipe_food.food.quantity
 
