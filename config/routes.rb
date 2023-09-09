87c0af8258e9c57e70486d6e68d701f263a8212c
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-09-09 17:08:08 +0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :food, only: [:index, :new, :create, :destroy]
   root "food#index"
 
   resources :food, only: [:index, :new, :create, :destroy]
+  resources :general_shopping_list, only: [:index]
   resources :recipes, only: [:index, :show, :new, :create, :destroy] do
     resources :recipe_food, only: [:new, :create]
   end
-  resources :recipe_foods, only: [:new, :create, :destroy]
-  resources :recipes, only: [:index, :show, :new, :create, :destroy]
   resources :recipe_food, only: [:destroy]
-  resources :general_shopping_list, only: [:index]
+  resources :public_recipes, only: [:index]
 end
